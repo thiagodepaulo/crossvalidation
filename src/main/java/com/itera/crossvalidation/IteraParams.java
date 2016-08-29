a88edd378984a1db04d52d7ca7c444868ea0e248
root <root@slave002>
2016-08-29 15:36:40 -0300

    super abstraction

---
+++ src/main/java/com/itera/crossvalidation/IteraParams.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.itera.crossvalidation;
+
+/**
+ *
+ * @author root
+ */
+public class IteraParams {
+    
+    public static int RUNS = 10;
+    
+}

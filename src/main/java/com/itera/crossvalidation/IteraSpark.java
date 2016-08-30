30e8ad7ae8d9d556a00ce1b6a0904babb17e0bad
root <root@master>
2016-08-30 10:35:13 -0300

    Loader

---
+++ src/main/java/com/itera/crossvalidation/IteraSpark.java
@@ -0,0 +1,59 @@
+package com.itera.crossvalidation;
+
+import java.io.Serializable;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.cassandra.CassandraSQLContext;
+
+
+public class IteraSpark implements Serializable {
+    
+    public transient static JavaSparkContext sc;
+    public static CassandraSQLContext sqlContext;
+
+    public static void configureContext(String jarLocation, String sparkHost, String appName, String cassandraHost, String cassandraUsername,
+            String cassandraPassword, String keyspace) {
+        System.out.println("Configuring Context ...");
+
+        SparkConf conf = new SparkConf(true)
+                .set("spark.cassandra.connection.host", cassandraHost)
+                .set("spark.cassandra.auth.username", cassandraUsername)
+                .set("spark.cassandra.auth.password", cassandraPassword);
+
+        String[] jars = new String[1];
+        jars[0] = jarLocation;
+
+        //Class[] classes = new Class[8];
+        //conf.registerKryoClasses(classes);
+        
+        conf.setJars(jars);
+
+        sc = new JavaSparkContext(sparkHost, appName, conf);
+
+        sqlContext = new CassandraSQLContext(sc.sc());
+        sqlContext.setKeyspace(keyspace);
+    }
+    
+    public static DataFrame queryCassandra(String tableName, String[] columns, String whereClause) {
+        String query = "";
+
+        if (columns != null) {
+            if (columns.length > 0) {
+                query = "SELECT " + String.join(",", columns) + " FROM " + tableName;
+            } else {
+                query = "SELECT * FROM " + tableName;
+            }
+        } else {
+            query = "SELECT * FROM " + tableName;
+        }
+
+        if (!whereClause.isEmpty()){
+            query += " WHERE " + whereClause + " ALLOW FILTERING";
+        }
+        
+        DataFrame df = sqlContext.cassandraSql(query);
+        
+        return df;
+    }
+}
--- src/main/java/com/itera/crossvalidation/Loader.java
+++ src/main/java/com/itera/crossvalidation/Loader.java
@@ -1,21 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.itera.crossvalidation;
 
-/**
- *
- * @author root
- */
+import Configuration.PreProcessing_Configuration;
+import IO.ListFiles;
+import Preprocessing.Cleaner;
+import Preprocessing.Preprocessing;
+import Preprocessing.StemPt.OrengoStemmer;
+import Preprocessing.StemPt.Stemmer;
+import Preprocessing.StemmerEn;
+import Preprocessing.StopWords;
+import static Preprocessing.TextRepresentation.RepresentTM;
+import Structures.Data;
+import Structures.FeatureList;
+import Structures.IndexValue;
+import Structures.InputPattern;
+import Word2Vec.JavaWord2Vec;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Row;
+import scala.Tuple2;
+
 public class Loader {
+
+    static DataFrame dataFrame; 
     
-    public Data static loadTrainFromCassandra() {
+    public static void loadData(String tableName, String[] columns, String whereClause){
+        dataFrame = IteraSpark.queryCassandra(tableName, columns, whereClause);
+    }
+    public static Data loadTrainFromCassandra(String language, String w2vDir) {
+        PreProcessing_Configuration configuration = new PreProcessing_Configuration(language, 2, true, false, true, true);
+
+        List<InputPattern> conteudos = dataFrame.javaRDD().map(new Function<Row, InputPattern>() {
+            @Override
+            public InputPattern call(Row row) throws Exception {
+                String id = row.getAs(row.fieldIndex("rsegda_lin_extrt"));
+                String classe = row.getAs(row.fieldIndex("category"));
+                String texto = row.getAs(row.fieldIndex("rsegda_lin_extrt"));
+
+                return new InputPattern(id, texto, classe);
+            }
+        }).collect();
+
+        Data data = RepresentTM(conteudos, configuration, w2vDir);
+
+        return data;
+    }
+
+    public static Data loadTestFromCassandra() {
         return null;
     }
     
-    public Data static loadTestFrom Cassandra() {
-        return null;
+    public static void loadDataFromCassandra(){
+        DataFrame[] dataFrames = dataFrame.randomSplit(new double[]  { 0.8, 0.2 } );
     }
 }
